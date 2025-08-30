83a9604bac2988195748ea0e7322a3a5589af170
IvetteVillanova <ivette.villanova2025@soymas.cl>
2025-08-30 17:22:44 -0400

    actualiza app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //Crear el array
+function listadoAmigos(){
+    let amigos = parseInt(document.getElementById('amigo').value);
 
-let amigos = parseInt(document.getElementById('amigo').value);
-
+    document.querySelectorAll('reiniciar')
 console.log(amigos);
+
+
+}
