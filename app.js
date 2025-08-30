694249e341b771e87b2f6ab24316f07166f2a7e8
IvetteVillanova <ivette.villanova2025@soymas.cl>
2025-08-30 17:49:02 -0400

    actualiza el app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function listadoAmigos(){
     let amigos = [];
     amigo = parseInt(document.getElementById('amigo').value);
 
-    document.querySelectorAll('reiniciar');
+    document.querySelectorAll('agregar');
     console.log(amigos);
 
 
